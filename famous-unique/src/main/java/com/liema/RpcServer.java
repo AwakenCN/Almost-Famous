4d4ae834e79bfd38f532d79773ca4a78610bcb87
noseparte <noseparte@aliyun.com>
2019-08-05 18:35:19 +0800

    thrift

---
+++ famous-unique/src/main/java/com/liema/RpcServer.java
@@ -0,0 +1,18 @@
+package com.liema;/**
+ * @author Noseparte
+ * @date 2019/8/5 16:48
+ * @Description
+ */
+
+/**
+ * @author Noseparte
+ * @date 2019/8/5 16:48
+ * @Description
+ */
+public class RpcServer {
+
+    public static void main(String[] args) {
+
+
+    }
+}
