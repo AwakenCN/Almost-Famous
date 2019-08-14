fe06d2f787f9e58f668b20e99c165e6311d03333
noseparte <noseparte@aliyun.com>
2019-08-14 11:50:44 +0800

    rpc config

---
+++ famous-common/src/main/java/com/liema/config/RpcConfig.java
@@ -0,0 +1,32 @@
+package com.liema.config;
+
+import com.liema.rpc.RpcClient;
+import com.liema.rpc.ThriftClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "rpc")
+public class RpcConfig {
+    private String host;
+    private int port;
+    private String rpcType;
+
+    @Bean(initMethod = "init")
+    public RpcClient rpcThriftClient() {
+        RpcClient rpcClient;
+        if (null != rpcType && "Thrift".equals(rpcType)) {
+            rpcClient = new ThriftClient();
+            rpcClient.setHost(host);
+            rpcClient.setPort(port);
+        } else {
+            rpcClient = new ThriftClient();
+            rpcClient.setHost(host);
+            rpcClient.setPort(port);
+        }
+        return rpcClient;
+    }
+}
