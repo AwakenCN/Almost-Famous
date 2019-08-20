231132a57bd46051c1d2e506c22519773ce9084f
noseparte <noseparte@aliyun.com>
2019-08-20 18:37:50 +0800

    FamousGameApplication

--- famous-game/src/main/java/com/liema/game/FamousGameApplication.java
+++ famous-game/src/main/java/com/liema/game/FamousGameApplication.java
@@ -1,14 +1,21 @@
 package com.liema.game;
 
+import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Noseparte
  * @date 2019/7/31 10:42
  * @Description
  */
-@SpringBootApplication
+@ImportAutoConfiguration(RedissonAutoConfiguration.class)
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@ComponentScan({"com.liema.game.*", "com.liema.common.*"})
 public class FamousGameApplication {
 
     public static void main(String[] args) {
