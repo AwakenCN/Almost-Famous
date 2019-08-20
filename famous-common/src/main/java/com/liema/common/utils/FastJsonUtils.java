d3d9d3f61982be6e693d9acc0bebd2d34d0bba0e
noseparte <noseparte@aliyun.com>
2019-08-20 18:42:03 +0800

    battleRank

--- famous-common/src/main/java/com/liema/common/utils/FastJsonUtils.java
+++ famous-common/src/main/java/com/liema/common/utils/FastJsonUtils.java
@@ -1,6 +1,9 @@
 package com.liema.common.utils;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
+import com.liema.common.bean.BattleRankBean;
 
 /**
  * @author Noseparte
@@ -11,5 +14,9 @@ public class FastJsonUtils {
 
     public static JSONObject parseObject(String json) {
         return JSONObject.parseObject(json);
+    }
+
+    public static <T> T toBean(String json, Class<T> clazz) {
+        return JSON.parseObject(json, clazz, Feature.OrderedField);
     }
 }
