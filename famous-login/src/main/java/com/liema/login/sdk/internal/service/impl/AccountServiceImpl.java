dadfea37900b423b069b876c0d786cad49a5be52
noseparte <noseparte@aliyun.com>
2019-08-20 11:36:43 +0800

    rpcConfig 注入

--- famous-login/src/main/java/com/liema/login/sdk/internal/service/impl/AccountServiceImpl.java
+++ famous-login/src/main/java/com/liema/login/sdk/internal/service/impl/AccountServiceImpl.java
@@ -9,6 +9,7 @@ import com.liema.common.utils.TokenGenerator;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
