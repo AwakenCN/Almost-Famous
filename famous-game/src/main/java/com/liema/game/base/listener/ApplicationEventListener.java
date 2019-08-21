ffd3de299e3f1516b04c9f485547c8b2379ba7a8
noseparte <noseparte@aliyun.com>
2019-08-21 18:22:31 +0800

    Almost-Login ApplicationEventListener 事件监听

--- famous-game/src/main/java/com/liema/game/base/listener/ApplicationEventListener.java
+++ famous-game/src/main/java/com/liema/game/base/listener/ApplicationEventListener.java
@@ -22,32 +22,32 @@ import org.springframework.context.event.ContextStoppedEvent;
  */
 public class ApplicationEventListener implements ApplicationListener {
 
-    public final static Logger LOGGER = LogManager.getLogger("GameCore");
+    public final static Logger LOG = LogManager.getLogger("GameCore");
 
     private ThriftClient thriftClient;
 
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
         if (event instanceof ApplicationEnvironmentPreparedEvent) {
-            LOGGER.debug("初始化环境变量");
+            LOG.debug("初始化环境变量");
         } else if (event instanceof ApplicationPreparedEvent) {
-            LOGGER.debug("初始化完成");
-            LOGGER.debug("初始GameData策划数据");
-            String path = ApplicationEventListener.class.getResource("/gamedata").getFile();
-            ConfigManager.loadGameData(path);
+            LOG.debug("初始化完成");
+            LOG.debug("初始GameData策划数据");
+//            String path = ApplicationEventListener.class.getResource("/gamedata").getFile();
+//            ConfigManager.loadGameData(path);
         } else if (event instanceof ContextRefreshedEvent) {
-            LOGGER.debug("应用刷新");
+            LOG.debug("应用刷新");
         } else if (event instanceof ApplicationReadyEvent) {
-            LOGGER.debug("应用已启动完成");
+            LOG.debug("应用已启动完成");
         } else if (event instanceof ContextStartedEvent) {
-            LOGGER.debug("应用启动，需要在代码动态添加监听器才可捕获");
+            LOG.debug("应用启动，需要在代码动态添加监听器才可捕获");
         } else if (event instanceof ContextStoppedEvent) {
-            LOGGER.debug("应用停止");
+            LOG.debug("应用停止");
         } else if (event instanceof ContextClosedEvent) {
             ApplicationContext applicationContext = ((ContextClosedEvent) event).getApplicationContext();
             thriftClient = applicationContext.getBean(ThriftClient.class);
             thriftClient.close();
-            LOGGER.debug("应用关闭");
+            LOG.debug("应用关闭");
         }
     }
 
