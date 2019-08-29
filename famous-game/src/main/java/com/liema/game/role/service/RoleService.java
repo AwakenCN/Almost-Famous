92b422f003ee90393971930b8acfb6d8a0788e4b
noseparte <noseparte@aliyun.com>
2019-08-29 18:12:05 +0800

    role 逻辑层

--- famous-game/src/main/java/com/liema/game/role/service/RoleService.java
+++ famous-game/src/main/java/com/liema/game/role/service/RoleService.java
@@ -1,4 +1,18 @@
 package com.liema.game.role.service;
 
+import com.liema.game.role.entity.Role;
+
 public interface RoleService {
+
+    Role selectById(Long uid);
+
+    Role selectByRoleId(long rid);
+
+    void newcomerGuide(Role role, Integer guideId);
+
+    void updateByRoleId(Role role);
+
+    void currencyChangeGM(Role role);
+
+
 }
--- famous-game/src/main/java/com/liema/game/role/service/impl/RoleServiceImpl.java
+++ famous-game/src/main/java/com/liema/game/role/service/impl/RoleServiceImpl.java
@@ -1,5 +1,6 @@
 package com.liema.game.role.service.impl;
 
+import com.liema.game.role.entity.Role;
 import com.liema.game.role.service.RoleService;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,28 @@ import org.springframework.stereotype.Service;
 @Service("roleService")
 public class RoleServiceImpl implements RoleService {
 
+    @Override
+    public Role selectById(Long uid) {
+        return null;
+    }
 
+    @Override
+    public Role selectByRoleId(long rid) {
+        return null;
+    }
 
+    @Override
+    public void newcomerGuide(Role role, Integer guideId) {
+
+    }
+
+    @Override
+    public void updateByRoleId(Role role) {
+
+    }
+
+    @Override
+    public void currencyChangeGM(Role role) {
+
+    }
 }
