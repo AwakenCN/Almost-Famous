59e790ca18f4d860f3cbd719440450b96897a57d
noseparte <noseparte@aliyun.com>
2019-08-30 15:55:56 +0800

    签到奖励的实体类

---
+++ famous-common/src/main/java/com/liema/common/bean/PropBean.java
@@ -0,0 +1,17 @@
+package com.liema.common.bean;
+
+import lombok.Data;
+
+/**
+ * @author Noseparte
+ * @date 2019/8/19 16:23
+ * @Description
+ */
+@Data
+public class PropBean {
+
+    public int itemId;
+    public int type;
+    public int num;
+
+}
--- famous-common/src/main/java/com/liema/common/exception/ErrorCode.java
+++ famous-common/src/main/java/com/liema/common/exception/ErrorCode.java
@@ -10,6 +10,15 @@ public enum ErrorCode {
     /*成功*/
     SERVER_SUCCESS(0),
 
+    /*客户端参数错误*/
+    CLIENT_PARAMS_ERROR(1),
+
+    /*token过期*/
+    TOKEN_EXPIRE_ERROR(2),
+
+    /*重名*/
+    REPEAT_NAME_ERROR(3),
+
     /*未知的协议号*/
     UNKNOWN_PROTOCOL(4),
 
@@ -19,7 +28,21 @@ public enum ErrorCode {
     /**
      * 账号不存在
      */
-    ACCOUNT_NOT_EXIST(1000);
+    ACCOUNT_NOT_EXIST(1000),
+
+    /**
+     * 获取签到信息失败
+     */
+    SIGN_LIST_ERROR(3004),
+    /**
+     * 签到失败
+     */
+    SIGN_ERROR(3005),
+    /**
+     * 时间未到，不可领取
+     */
+    REWARD_UNAVAILABLE_TIME(3006);
+
 
     ErrorCode(int i) {
         this.value = i;
