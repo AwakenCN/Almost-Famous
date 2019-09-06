5bafa22cb5ffeb82f103dd506297680417f171ff
noseparte <noseparte@aliyun.com>
2019-09-06 19:59:46 +0800

    背包列表

--- famous-game/src/main/java/com/liema/game/pack/controller/CardBagListAction.java
+++ famous-game/src/main/java/com/liema/game/pack/controller/CardBagListAction.java
@@ -2,7 +2,7 @@ package com.liema.game.pack.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.liema.common.bean.Action;
-import com.liema.common.bean.Resoult;
+import com.liema.common.global.Resoult;
 import com.liema.game.base.RegisterProtocol;
 import com.liema.game.pack.entity.ActorBag;
 import com.liema.game.pack.service.BagCardStrategyService;
