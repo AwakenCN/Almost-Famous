05bf802f21f06d4f9370c285c7f3182bd17f8a8c
noseparte <noseparte@aliyun.com>
2019-09-07 11:56:53 +0800

    异步请求

---
+++ famous-battle/src/main/java/com/liema/battle/match/SMatchCancel.java
@@ -0,0 +1,10 @@
+package com.liema.battle.match;
+
+import com.liema.battle.server.Protocol;
+
+public class SMatchCancel extends Protocol {
+    @Override
+    protected void process() throws Exception {
+
+    }
+}
