098ee52219b12822a7ec828e5baf150b4534ebec
noseparte <noseparte@aliyun.com>
2019-09-10 11:54:51 +0800

    SignReward

--- famous-game/src/main/java/com/liema/game/sign/entity/SignReward.java
+++ famous-game/src/main/java/com/liema/game/sign/entity/SignReward.java
@@ -5,11 +5,13 @@ import com.liema.common.db.pojo.GeneralBean;
 import com.liema.common.global.Misc;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
+@Document(collection = "famous-game-sign-reward")
 public class SignReward extends GeneralBean {
 
     private Long rid;
