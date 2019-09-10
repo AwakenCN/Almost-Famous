0435ea3adf18a1e3efd616c81b343f4dd0682e30
noseparte <noseparte@aliyun.com>
2019-09-10 11:56:59 +0800

    createRole

--- famous-game/src/main/java/com/liema/game/pack/mongo/ActorBagDao.java
+++ famous-game/src/main/java/com/liema/game/pack/mongo/ActorBagDao.java
@@ -1,26 +1,15 @@
 package com.liema.game.pack.mongo;
 
+import com.liema.common.db.dao.GeneralDao;
 import com.liema.game.pack.entity.ActorBag;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Repository;
-
-import javax.annotation.Resource;
-
-@Repository
-public class ActorBagDao {
-
-    @Resource
-    @Qualifier("gameMongoTemplate")
-    private MongoTemplate gameMongoTemplate;
-
-    public void updateActorBag(ActorBag pack){}
 
 
-    public void insertActorBag(ActorBag card){
+public interface ActorBagDao extends GeneralDao<ActorBag> {
 
-    }
+    void updateActorBag(ActorBag pack);
 
-    public ActorBag getSpecificBackpack(Long rid){ return null;}
+    void insertActorBag(ActorBag card);
+
+    ActorBag getSpecificBackpack(Long rid);
 
 }
