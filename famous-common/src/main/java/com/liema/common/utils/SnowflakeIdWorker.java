be1a372ff6c94c4b771968d568b5e7790279008d
noseparte <noseparte@aliyun.com>
2019-09-21 14:46:04 +0800

    = CI 雪花算法 Unable to find a single main class from the following candidates

--- famous-common/src/main/java/com/liema/common/utils/SnowflakeIdWorker.java
+++ famous-common/src/main/java/com/liema/common/utils/SnowflakeIdWorker.java
@@ -143,12 +143,12 @@ public final class SnowflakeIdWorker {
 
     //==============================Test=============================================
 //    /** 测试 */
-    public static void main(String[] args) {
-        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0);
-        for (int i = 0; i < 1000; i++) {
-            long id = idWorker.nextId();
-            System.out.println(Long.toBinaryString(id));
-            System.out.println(id);
-        }
-    }
+//    public static void main(String[] args) {
+//        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0);
+//        for (int i = 0; i < 1000; i++) {
+//            long id = idWorker.nextId();
+//            System.out.println(Long.toBinaryString(id));
+//            System.out.println(id);
+//        }
+//    }
 }
