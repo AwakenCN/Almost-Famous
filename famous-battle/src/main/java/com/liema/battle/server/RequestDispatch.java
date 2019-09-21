f95c46c6287525e2fdc9480ed5483fd2d92c464e
noseparte <noseparte@aliyun.com>
2019-09-21 14:49:07 +0800

    接战斗服务器查询是否战斗未完成断线的情况，如果在战斗中发匹配协议会补发战斗房间信息

---
+++ famous-battle/src/main/java/com/liema/battle/server/RequestDispatch.java
@@ -0,0 +1,13 @@
+package com.liema.battle.server;
+
+/**
+ * @author Noseparte
+ * @date 2019/8/23 15:57
+ * @Description
+ */
+public class RequestDispatch {
+
+    public static void dispatch(Protocol msg, int sid) {
+
+    }
+}
---
+++ famous-battle/src/main/java/com/liema/battle/utils/BattleRoomBroadcastJob.java
@@ -0,0 +1,12 @@
+package com.liema.battle.utils;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class BattleRoomBroadcastJob implements Job {
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+    }
+}
