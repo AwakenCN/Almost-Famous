6036e4da737f2370afe0314c832ac22824304e5c
noseparte <noseparte@aliyun.com>
2019-12-18 12:36:49 +0800

    /= App Token策略的权限设计

---
+++ famous-common/src/main/java/com/noseparte/common/bean/SecurityEnum.java
@@ -0,0 +1,17 @@
+package com.noseparte.common.bean;
+
+/**
+ * @Auther: Noseparte
+ * @Date: 2019/12/4 16:55
+ * @Description: 
+ * 
+ *          <p>安全协议</p>
+ */
+public enum SecurityEnum {
+
+    /**/
+    Credential,
+    /**/
+    API
+
+}
