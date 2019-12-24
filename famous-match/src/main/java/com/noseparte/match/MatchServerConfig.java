ec96375e82b23ec580eb2e500aada687a6a1819c
noseparte <noseparte@aliyun.com>
2019-12-24 22:37:02 +0800

    /= match服 加载系统配置类

--- famous-match/src/main/java/com/noseparte/match/MatchServerConfig.java
+++ famous-match/src/main/java/com/noseparte/match/MatchServerConfig.java
@@ -5,6 +5,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+
+/**
+ * @Auther: Noseparte
+ * @Date: 2019-12-24 22:36
+ * @Description:
+ *
+ *          <p>match服</p>
+ *          <p>系统参数</p>
+ */
 @Data
 @Component
 @Order(1)
@@ -16,7 +25,7 @@ public class MatchServerConfig {
     private int matcher;    // 匹配人数
     private int lifecycle;// 房间定时器生命周期单位分钟
     private int verifyUserLoginCmd;// 验证玩家登录
-    private int pullActorCmd;   // 从gamecore服务器拉取角色信息
+    private int pullActorCmd;   // 从game服务器拉取角色信息
     private int threadCount;    // 线程池数量
     private int heartBeatTime;// 连接最大空闲时间
     private int matchMaxWaitTime;// 匹配队列中最大等待时间
